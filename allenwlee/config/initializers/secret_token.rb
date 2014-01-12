0ae9e293ae90eedfb9a5cc8cf6c938c549ed36f4
allenwlee <allenwlee@Allens-MacBook-Pro.local>
2014-01-11 23:04:09 -0800

    rails 3 deleted rails 4 installed

---
+++ allenwlee/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Allenwlee::Application.config.secret_token = 'c7ebe34cdd8b9eb7b5a42c46d7746954f41546918c4d6aa67d3305a0bc5b58685c35ad4a182865f1cc5047cd8d15dc42900c9207d980914dc8adcb91120618ea'
